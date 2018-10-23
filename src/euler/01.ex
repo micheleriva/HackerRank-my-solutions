f1c62c2dfdffc32a8c3730a63a0485794c01e61f
Michele Riva <master@micheleriva.it>
2018-10-23 17:24:52 +0200

    fixed elixir euler 1

--- src/euler/01.ex
+++ src/euler/01.ex
@@ -1,9 +1,27 @@
 defmodule Solution do
 
-  {number, _} = :string.to_integer(IO.read(:stdio, :line))
+  def compute_reminder(number) do
+    if rem(number, 3) == 0 or rem(number, 5) == 0, do: number, else: 0
+  end
 
-  Enum.map(1..number, fn n -> if(rem(n, 3) == 0 or rem(n, 5) == 0, do: n, else: 0) end)
-          |> Enum.sum
-          |> IO.puts
+  def compute(limit) do
+    max = limit - 1
+    Enum.map(1..max, &(compute_reminder(&1)))
+        |> Enum.sum
+        |> IO.puts
+  end
+
+  def get_io do
+    IO.gets("") 
+      |> String.trim
+      |> String.to_integer
+  end
+
+  def solve do
+    test_cases = get_io()
+    Enum.map(1..test_cases, fn(_) -> get_io() |> compute() end)
+  end
 
 end
+
+Solution.solve

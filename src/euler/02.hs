fe6e94e0913911889f08c9e471e1815a1465f15a
Michele Riva <master@micheleriva.it>
2018-10-24 12:27:33 +0200

    wip on euler2 in haskell

--- src/euler/02.hs
+++ src/euler/02.hs
@@ -1,15 +1,15 @@
 import Control.Monad.State
 
-fibo n = flip.evalState (0,1) $ do
-  forM_ [0..(n-1)] $ \_ -> do
-    (a,b) <- get
-    put (b,a+b)
-  (a,b) <- get
-  return a
+fibo :: Integer -> Integer
+fibo 0 = 0
+fibo 1 = 1
+fibo n = fibo (n-1) + fibo (n-2)
+
+isEven :: Integer -> Bool
+isEven x = x `mod` 2 == 0
 
 main = do
   cases <- readLn
   forM_ [1..cases] $ \_ -> do
     limit <- readLn
     print $ sum $ map (\n -> if isEven (fibo n) then n else 0) [1..limit]
-    where isEven x = x `mod` 2 == 0
